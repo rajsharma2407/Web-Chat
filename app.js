30cb094bf07f1bfdb026598095ad360271b0802a
Raj Sharma <sharmaraj22221@gmail.com>
2020-12-18 22:04:15 +0530

    initial

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const PORT = process.env.port || 8080;
+const {v4:uuidv4} = require('uuid');
+
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+io.on('connection',socket=>{
+
+    socket.broadcast.emit('user','new user connected');
+        socket.on('message',(date,time,msg,id)=>{
+            io.emit('newMessage',date,time,msg,id);
+        })
+        socket.on('join-room',(roomId,id)=>{
+            socket.join(roomId);
+            socket.to(roomId).broadcast.emit('user-connected',id);
+            
+            socket.on('disconnect',()=>{
+                io.to(roomId).emit('user-disconnected',id);
+            })
+            socket.on('dis-connect',()=>{
+                socket.to(roomId).broadcast.emit('user-disconnected',id);
+            })
+        })
+})
+
+app.get('/',(req,res)=>{
+    res.redirect('/chat/'+uuidv4());
+})
+
+app.get('/chat/:room',(req,res)=>{
+    res.render('index',{id:req.params.room})
+})
+app.get('/video/:room',(req,res)=>{
+    res.render('video',{id:req.params.room});
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+server.listen(PORT,console.log('server is started at port '+PORT));
---
+++ public/script.js
@@ -0,0 +1,92 @@
+const socket = io('/');
+const btn = document.querySelector('.btn');
+const text = document.querySelector('#text');
+const msgBox = document.querySelector('.msg-box');
+
+socket.on('user',msg=>{
+
+    let today = new Date();
+    let date = today.toLocaleDateString("hi-IN");
+    let newTime = today.toLocaleTimeString("hi-IN");
+    
+    addMessage(date, newTime, msg, 'newuser');
+})
+
+btn.onclick = function(){
+    
+    let msg = text.value;
+    if(msg.trim()!=""){
+    let today = new Date();
+    let date = today.toLocaleDateString("hi-IN");
+    let newTime = today.toLocaleTimeString("hi-IN");
+    
+   
+    socket.emit('message',date, newTime, msg, myId);
+
+    text.value = "";
+    text.focus();
+    }
+
+}
+
+text.addEventListener('keyup',function(e){
+
+    if(e.keyCode == 13){
+    let msg = text.value;
+    if(msg.trim()!=""){
+    let today = new Date();
+    let date = today.toLocaleDateString("hi-IN");
+    let newTime = today.toLocaleTimeString("hi-IN");
+    
+   
+    socket.emit('message',date, newTime, msg, myId);
+
+    text.value = "";
+    text.focus();
+    }
+}
+})
+
+socket.on('newMessage',(date, time, msg,userId)=>{
+    addMessage(date,time, msg, userId);
+
+})
+const addMessage = (date, time, msg, userId)=>{
+    var dateDiv = document.createElement('div');
+    var dateSpan = document.createElement('span');
+    var timeSpan = document.createElement('span');
+    var msgDiv = document.createElement('div');
+    var msgP = document.createElement('p');
+    var mainDiv = document.createElement('div');
+
+    if(userId === myId || userId == 'newuser')
+    mainDiv.style.float = "left";
+    else
+    mainDiv.style.float = "right";
+    mainDiv.style.clear="both"
+    dateSpan.innerText = date;
+    timeSpan.innerText = time;
+    msgP.innerText = msg;
+    msgDiv.appendChild(msgP);
+    dateDiv.appendChild(dateSpan);
+    dateDiv.appendChild(timeSpan);
+    mainDiv.appendChild(dateDiv);
+    mainDiv.appendChild(msgDiv);
+
+    //styles 
+    dateDiv.style.margin="10px 0"
+    mainDiv.style.display="flex";
+    mainDiv.style.flexDirection="column";
+    mainDiv.style.margin="10px"
+    mainDiv.style.padding="15px"
+    mainDiv.style.maxWidth="60%"
+    msgP.style.maxWidth="100%"
+    mainDiv.style.background="white";
+    mainDiv.style.borderRadius="18px";
+    dateDiv.style.display="flex";
+    dateDiv.style.justifyContent="space-between";
+
+
+    msgBox.appendChild(mainDiv);
+    msgBox.scrollTop = msgBox.scrollHeight;
+}
